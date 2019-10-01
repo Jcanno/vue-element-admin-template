f126a3d1023a929f8c973a9c0245151d7e653732
金志强 <haoxihaoderen@163.com>
2019-10-01 21:31:14 +0800

    f-add styles

--- src/filters/dateTransToTimestamp.js
+++ src/filters/dateTransToTimestamp.js
@@ -4,6 +4,8 @@ import moment from 'moment';
  * 将日期转换为时间戳过滤器
  * @param {string} datestr 日期字符串
  */
-export const DateTransToTimestamp = datestr => {
+const DateTransToTimestamp = datestr => {
   return parseInt(moment(datestr).valueOf() / 1000);
 };
+
+export default DateTransToTimestamp;
