ec97997c85c185987b49d8a41278868df419004f
金志强 <haoxihaoderen@163.com>
2019-10-02 14:18:33 +0800

    test: 测试没有被格式化的提交

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   // 单行最大字符数
   printWidth: 120,
-  // 使用tab缩进，默认false
+  // 使用tab缩进
   useTabs: true,
+  // 对象或数组最后项不添加逗号
   trailingComma: 'none',
   // tab 两个空格
   tabWidth: 2,
   // 允许分号
   semi: true,
   // 使用单引号
-  singleQuote: true,
-  jsxBracketSameLine: false
+  singleQuote: true
 };
