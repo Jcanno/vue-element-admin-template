fe1fbc7566a6e4b31097e5c8213fdc07e8707697
金志强 <haoxihaoderen@163.com>
2019-10-03 17:42:10 +0800

    feat: 增加webpack长效缓存

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,56 @@ module.exports = {
 		}
 	},
 	//配置目录别名
-	chainWebpack: config => {
-		config.resolve.alias.set('@', resolve('src')).set('_c', resolve('src/components'));
+	configureWebpack: {
+		name: '',
+		resolve: {
+			alias: {
+				'@': resolve('src'),
+				_c: resolve('src/components')
+			}
+		}
+	},
+	chainWebpack(config) {
+		// set preserveWhitespace
+		config.module
+			.rule('vue')
+			.use('vue-loader')
+			.loader('vue-loader')
+			.tap(options => {
+				options.compilerOptions.preserveWhitespace = true;
+				return options;
+			})
+			.end();
+
+		config
+			// https://webpack.js.org/configuration/devtool/#development
+			.when(process.env.NODE_ENV === 'development', config => config.devtool('cheap-source-map'));
+
+		config.when(process.env.NODE_ENV !== 'development', config => {
+			config.optimization.splitChunks({
+				chunks: 'all',
+				cacheGroups: {
+					libs: {
+						name: 'chunk-libs',
+						test: /[\\/]node_modules[\\/]/,
+						priority: 10,
+						chunks: 'initial'
+					},
+					elementUI: {
+						name: 'chunk-elementUI',
+						priority: 20,
+						test: /[\\/]node_modules[\\/]_?element-ui(.*)/
+					},
+					commons: {
+						name: 'chunk-commons',
+						test: resolve('src/components'),
+						minChunks: 3,
+						priority: 5,
+						reuseExistingChunk: true
+					}
+				}
+			});
+			config.optimization.runtimeChunk('single');
+		});
 	}
 };
