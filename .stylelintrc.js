887826d8b6c5b4b5e9ff0c4c4e11b0f4e265b021
金志强 <haoxihaoderen@163.com>
2019-10-03 08:09:11 +0800

    style: 更改style规则

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,51 +7,34 @@ module.exports = {
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "property-no-unknown": [
-      true,
-      {
-        "ignoreProperties": [
-          "composes"
-        ]
-      }
-    ],
-    "selector-pseudo-class-no-unknown": [
-      true,
-      {
-        "ignorePseudoClasses": [
-          "global"
-        ]
-      }
-    ],
-    "scss/dollar-variable-pattern": "^foo",
-    "scss/selector-no-redundant-nesting-selector": true,
-    "declaration-empty-line-before": [
-      "always",
-      {
-        "except": [
-          "after-declaration",
-          "first-nested"
-        ],
-        "ignore": [
-          "after-comment",
-          "inside-single-line-block"
-        ]
-      }
-    ],
-    "block-no-empty": null,
+    // 不允许空块
+    "block-no-empty": true,
+    // 不允许无效的颜色值
     "color-no-invalid-hex": true,
     "comment-empty-line-before": [ "always", {
-       "ignore": ["stylelint-commands", "after-comment"]
+      "ignore": ["stylelint-commands", "after-comment"]
     }],
+    // 冒号后需要空格
     "declaration-colon-space-after": "always",
-    "indentation": ["tab", {
-      "except": ["value"]
+    // tab缩进
+    "indentation": 2,
+    // 每个规则前需要空一行
+    "at-rule-empty-line-before": ["always", {
+      "except": ["after-same-name", "inside-block", "blockless-after-same-name-blockless", "blockless-after-blockless"]
     }],
+    // 单位使用小写
+    "unit-case": "lower",
+    // 使用单引号
+    "string-quotes": "single",
+    // 最大空行
     "max-empty-lines": 1,
     "rule-empty-line-before": [ "always", {
       "except": ["first-nested"],
       "ignore": ["after-comment"]
     } ],
+    // 多个选择器需要换行
+    "selector-list-comma-newline-after": "always",
+    // 单位白名单
     "unit-whitelist": ["px", "em", "rem", "%", "s"]
   }
 }
