0057fc39fd503c816bfafdba25b66806bbab93ca
金志强 <haoxihaoderen@163.com>
2019-10-03 17:33:34 +0800

    test: 测试提交

---
+++ src/components/Example/Example.vue
@@ -0,0 +1,23 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: 'example',
+
+	components: {},
+
+	data() {
+		return {};
+	},
+
+	methods: {},
+
+	computed: {},
+
+	created() {},
+
+	mounted() {}
+};
+</script>
