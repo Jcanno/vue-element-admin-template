1563550e3c64b2c7c23d2e1c7240cf3d50641836
金志强 <haoxihaoderen@163.com>
2019-10-24 22:44:32 +0800

    docs: 更新文档

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     "indentation": "tab",
     // 关闭对scss @的未知验证，兼容scss
     "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "scss/at-each-key-value-single-line": true,
+		"scss/at-rule-no-unknown": true,
+		// @ 规则之后不需要空格
+		"at-rule-name-space-after": null
   }
 }
