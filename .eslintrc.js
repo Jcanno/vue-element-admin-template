8a9d75a1f8913a3971bedb0ce40da27ee57f36e2
金志强 <haoxihaoderen@163.com>
2019-10-25 08:26:58 +0800

    style: 更改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
   env: {
-    es6: true, // 启用 ES6 语法支持以及新的 ES6 全局变量或类型
-    node: true, // Node.js 全局变量和 Node.js 作用域
-    browser: true, // 浏览器全局变量
+		// 启用 ES6 语法支持以及新的 ES6 全局变量或类型
+		es6: true, 
+		// Node.js 全局变量和 Node.js 作用域
+		node: true, 
+		// 浏览器全局变量
+    browser: true, 
   },
   extends: [
     "eslint:recommended",
